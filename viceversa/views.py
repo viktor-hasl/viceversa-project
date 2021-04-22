f5c47fea5ae564240c6c71a8d4e038fdf5ebb9d2
Viktor <papenson622@gmail.com>
2021-04-22 14:43:17 +0300

    мое первое приложение готово

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def revers(request):
+    user_text = request.GET['user_text']
+    revers_text = user_text[::-1]
+    kol = len(user_text.split())
+    return render(request, 'revers.html', {'use': user_text, 'rev': revers_text, 'k': kol})
